0698c08dc31586feca2caa15ba3edba0360b2203
Wesley Banfield <wesleybanfield@gmail.com>
2021-03-13 15:42:57 +0100

    Add Basemap to homepage

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <BaseMap/>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
-import HelloWorld from './components/HelloWorld.vue'
+import BaseMap from './components/BaseMap.vue'
 
 @Options({
   components: {
-    HelloWorld
+    BaseMap
   }
 })
 export default class App extends Vue {}
---
+++ src/components/BaseMap.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id='mapContainer' class='basemap'></div>
+</template>
+
+<script>
+import mapboxgl from 'mapbox-gl'
+
+export default {
+  name: 'BaseMap',
+  data () {
+    return {
+      // This token was taken from the demo project we need to replace with a real token
+      accessToken: 'pk.eyJ1Ijoid2VzbGV5YmFuZmllbGQiLCJhIjoiY2pmMDRwb202MGlzNDJ3bm44cHA3YXZiNCJ9.b2yOf2vbWnWiV7mlsFAywg'
+    }
+  },
+  mounted () {
+    mapboxgl.accessToken = this.accessToken
+
+    const map = new mapboxgl.Map({
+      container: 'mapContainer',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [7.5956888, 41.4316886],
+      zoom: 3.5
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.basemap {
+  width: 95vw;
+  height: 60vh;
+}
+</style>
