40f9db00efce77b371e0e4bf54a4d36f4089b8e8
Jean-Baptiste Regazzoni <jeanbaptiste.regazzoni@protonmail.com>
2021-03-16 23:58:55 +0100

    commit avancement header ajout tailwindcss modification règles lint (console et debugger interdits en production)

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <Header />
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
@@ -6,22 +7,13 @@
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 @Options({
   components: {
+    Header,
     HelloWorld
   }
 })
 export default class App extends Vue {}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+<!-- TODO Faire le CSS mobile first -->
+  <header>
+    <div class="flex">
+      <div class="flex-none flex">
+        <div>
+          <p class="text-xl leading-6">Carte Interactive</p>
+          <p class="text-lg leading-4">Historique des activités et observations des secours en Méditerranée</p>
+        </div>
+        <img class="h-10" alt="Vue logo" src="./../assets/logo.png" />
+      </div>
+      <div class="flex-grow"></div>
+      <div class="flex-none flex-col">
+        <p class="text-center text-2xl font-black leading-6">SOS</p>
+        <p class="leading-4">Méditerranée</p>
+      </div>
+    </div>
+    <nav class="bg-indigo-900 text-white flex h-10 items-center">
+      <div class="flex-none flex">
+        <a href="#" class="px-1 flex-initial inline-block">Ongoing</a>
+        <a href="#" class="px-1 flex-initial inline-block">Past SAR events</a>
+        <a href="#" class="px-1 flex-initial inline-block">Our mission</a>
+        <a href="#" class="px-1 flex-initial inline-block">Legal references</a>
+        <a href="#" class="px-1 flex-initial inline-block">Glossary</a>
+        <a href="#" class="px-1 flex-initial inline-block">FAQ</a>
+      </div>
+      <div class="flex-grow"></div>
+      <div class="flex-none"><a href="#" class="text-center inline-block w-6" style="background-color: #ffffff;color: #000000">?</a></div>
+    </nav>
+  </header>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component'
+
+export default class Header extends Vue {}
+</script>
