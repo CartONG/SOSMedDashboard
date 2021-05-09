36d848a94a62c17f4cd8fbfbf091085f2ecec055
Jean-Baptiste Regazzoni <jeanbaptiste.regazzoni@protonmail.com>
2021-05-09 23:54:07 +0200

    fix(header): responsive header available

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,55 +2,61 @@
   <header>
     <div class="lg:hidden">
       <div class="flex">
-        <div class="flex-none flex-col mt-4 ml-2 text-indigo-900">
-          <p class="text-center text-2xl font-black leading-6">SOS</p>
-          <p class="leading-4">Méditerranée</p>
+        <div class="mt-4 ml-2 text-main leading-3 font-black">
+          <p class="text-center">SOS</p>
+          <p class="text-tiny uppercase">Mediterranee</p>
         </div>
-        <div class="flex-shrink w-4"></div>
-        <div class="flex-1 flex mt-2">
-          <div class="flex-1">
-            <p class="text-lg leading-6 text-indigo-900 font-medium text-justify uppercase">Carte Interactive</p>
-            <p class="text-xs leading-tight text-yellow-600 text-opacity-100 font-medium">
-              Historique des activités et observations
-              des secours en Méditerranée
-            </p>
-          </div>
+        <div class="flex-shrink-0 flex-grow w-2"></div>
+        <div class="flex-auto">
+          <p class="text-main uppercase">Carte Interactive</p>
+          <p class="text-tiny leading-tight text-secondary">
+            Historique des activités et observations
+            des secours en Méditerranée
+          </p>
         </div>
-        <div class="flex-shrink w-4"></div>
-        <svg class="flex-none pt-4 pl-4 cursor-pointer text-indigo-900 fill-current h-16 w-16" viewBox="0 0 100 80" width="40" height="40">
+        <div class="flex-shrink-0 flex-grow w-2"></div>
+        <svg class="cursor-pointer text-main fill-current w-14" :class="{hidden: menuVisibility}"
+             viewBox="0 0 90 60" @click="setMenuVisibility(true)">
           <rect width="80" height="15" rx="8"></rect>
           <rect y="20" width="80" height="15" rx="8"></rect>
           <rect y="40" width="80" height="15" rx="8"></rect>
         </svg>
+        <svg class="cursor-pointer text-main fill-current w-14" :class="{hidden: !menuVisibility}"
+             viewBox="0 0 90 60" @click="setMenuVisibility(false)">
+          <line x1="15" y1="5" x2="65" y2="55" stroke="currentColor" stroke-width="15" stroke-linecap="round"
+                stroke-linejoin="round"/>
+          <line x1="65" y1="5" x2="15" y2="55" stroke="currentColor" stroke-width="15" stroke-linecap="round"
+                stroke-linejoin="round"/>
+        </svg>
       </div>
-      <div class="bg-blue-900 bg-opacity-100 h-10">
+      <div class="bg-main h-10">
       </div>
     </div>
     <div class="hidden lg:block">
-      <div class="flex">
+      <div class="flex my-4 ml-8 ">
         <div class="flex-none flex">
           <div>
-            <p class="text-xl leading-6">Carte Interactive</p>
-            <p class="text-lg leading-4">
+            <p class="text-xl leading-6 text-main">Carte Interactive</p>
+            <p class="text-base leading-4 text-secondary">
               Historique des activités et observations des secours en
               Méditerranée
             </p>
           </div>
-          <img class="h-10" alt="Vue logo" src="./../assets/logo.png" />
+          <img class="h-10" alt="Vue logo" src="./../assets/logo.png"/>
         </div>
         <div class="flex-grow"></div>
-        <div class="flex-none flex-col">
-          <p class="text-center text-2xl font-black leading-6">SOS</p>
-          <p class="leading-4">Méditerranée</p>
+        <div class="flex-none">
+          <p class="text-center font-black leading-5">SOS</p>
+          <p class="font-black leading-5">MEDITERRANEE</p>
         </div>
       </div>
-      <nav class="bg-indigo-800 text-white flex h-10 items-center">
+      <nav class="bg-main text-white flex h-10 items-center">
         <div class="flex-none flex">
           <a href="#" class="px-1 flex-initial inline-block">Ongoing</a>
           <a href="#" class="px-1 flex-initial inline-block">Past SAR events</a>
           <a href="#" class="px-1 flex-initial inline-block">Our mission</a>
           <a href="#" class="px-1 flex-initial inline-block"
-            >Legal references</a
+          >Legal references</a
           >
           <a href="#" class="px-1 flex-initial inline-block">Glossary</a>
           <a href="#" class="px-1 flex-initial inline-block">FAQ</a>
@@ -61,7 +67,7 @@
             href="#"
             class="text-center inline-block w-6"
             style="background-color: #ffffff; color: #000000"
-            >?</a
+          >?</a
           >
         </div>
       </nav>
@@ -72,5 +78,11 @@
 <script lang='ts'>
 import { Vue } from 'vue-class-component'
 
-export default class Header extends Vue {}
+export default class Header extends Vue {
+  menuVisibility = false;
+
+  setMenuVisibility (isVisible: boolean) {
+    this.menuVisibility = isVisible
+  }
+}
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        main: '#1a2747',
+        secondary: '#f03e1b',
+        textSubtitles: '#3c3c3b',
+        blockBackground: '#e3e2e9'
+      },
+      fontFamily: {
+        sans: 'raleway'
+      },
+      fontSize: {
+        tiny: ['0.65rem', '0.75rem']
+      }
+    }
   },
   variants: {
     extend: {}
