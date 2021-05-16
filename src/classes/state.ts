81bc63179c618dd7a1e62823fccc17e9beb08b57
NicolasGrosjean <grosjen@hotmail.fr>
2021-05-16 16:27:49 +0200

    Fix initial date
    In the date constructor, months starts at 0

--- src/classes/state.ts
+++ src/classes/state.ts
@@ -5,7 +5,7 @@ import { HistogramSlider } from '../classes/HistogramSlider'
 // State class which keeps the current state of the application
 export class State {
   timeFilteredData: OpsData[] = [];
-  minDate = new Date(2016, 1, 1);
+  minDate = new Date(2016, 0, 1);
   maxDate = new Date();
   baseMap = new BaseMap();
   histogramSlider = new HistogramSlider()
