b3ace350537bacfa65e100bcadfe45017130797e
Jean-Baptiste Regazzoni <jeanbaptiste.regazzoni@protonmail.com>
2022-03-22 23:15:40 +0100

    :sparkles: improve donation header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -30,8 +30,8 @@
           <i class="ml-14 text-8xl leading-6 icon icon-sosmed-ship"></i>
         </div>
         <div class="flex-grow"></div>
-        <div @click="showForm = !showForm" class="bg-secondary text-donationText cursor-pointer w-60">
-          <div class="p-4">
+        <div @click="showForm = !showForm" class="bg-secondary text-donationText cursor-pointer w-52">
+          <div class="p-2">
             <p class="text-center text-2xl">Sauver des vies</p>
             <div class="flex justify-center items-center">
               <span class="icon icon-down text-xs" :class="{'up-arrow': showForm, 'pl-1': showForm, 'pr-1': !showForm}"></span>
@@ -42,7 +42,7 @@
           <form action="https://don.sosmediterranee.org"
                 class="z-100 absolute transition-[max-height] ease-in-out duration-500 overflow-hidden"
                 :style="formStyle">
-            <div class="p-4 bg-secondary w-60 text-xl font-black">
+            <div class="px-5 py-6 bg-secondary w-52 text-xl font-blac flex flex-col">
               <input name="utm_source" type="hidden" value="sitesosmediterranee">
               <input name="utm_medium" type="hidden" value="site">
               <input name="utm_campaign" type="hidden" value="don_site_je_donne">
@@ -51,23 +51,23 @@
                   <input type="radio" name="amount" value="30"> 30 €
                 </label>
               </div>
-              <div class="radio text-center">
+              <div class="text-center p-1">
                 <label class="amount">
                   <input type="radio" name="amount" value="50"> 50 €
                 </label>
               </div>
-              <div class="radio text-center">
+              <div class="text-center pb-4">
                 <label class="amount">
                   <input type="radio" name="amount" value="100"> 100 €
                 </label>
               </div>
-              <input class="bg-main text-uppercase w-40 m-8 mb-2 p-4 font-black" type="submit" value="Je donne">
+              <input class="bg-main text-uppercase p-4 font-black" type="submit" value="Je donne">
             </div>
           </form>
         </div>
         <div class="flex-none mt-4 mr-4 ml-4" onclick="location.href='https://www.sosmediterranee.fr/';"
              style="cursor: pointer">
-          <p class="text-center font-black leading-5">SOS</p>
+          <p class="text-center font-black leading-5 text-3xl">SOS</p>
           <p class="font-black leading-5">MEDITERRANEE</p>
         </div>
       </div>
