fced67b804a2d983cb6bf569b1a721e232dd6ff8
Etienne Delclaux <e_delclaux@cartong.org>
2022-05-06 17:20:59 +0200

    feat (i18n): translate legend

--- src/components/Legend.vue
+++ src/components/Legend.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="flex flex-col rounded-2xl w-1/2 legend-background absolute top-24 ml-4 legend-position-mobile sm:w-60 md:right-5 md:absolute md:bottom-5 md:top-auto">
-    <p class="text-xs text-center font-bold text-secondary">VIEW</p>
-    <Switch switch-id='rescue' switch-title="Rescue" />
-    <Switch switch-id='transfer' switch-title="Transfer" />
-    <Switch switch-id='medical' switch-title="Medical Evacuation" />
+    <p class="text-xs text-center font-bold text-secondary">{{ $t("legend.title").toUpperCase() }}</p>
+    <Switch switch-id='rescue' :switch-title="$t('legend.rescue')" />
+    <Switch switch-id='transfer' :switch-title="$t('legend.transfer')" />
+    <Switch switch-id='medical' :switch-title="$t('legend.med')" />
     <svg class="h-7" viewBox="0 0 100 40">
       <line x1="0" y1="20" x2="100" y2="20" stroke="black" />
     </svg>
-    <Switch switch-id='harbor' switch-title="Harbors" />
+    <Switch switch-id='harbor' :switch-title="$t('legend.harbors')" />
   </div>
 </template>
 
