8cff9a1a8e400dbd71e2b4de9ec3269aa0061822
Etienne Delclaux <e_delclaux@cartong.org>
2022-05-06 18:46:37 +0200

    use const in basemap.ts

--- src/classes/BaseMap.ts
+++ src/classes/BaseMap.ts
@@ -1,19 +1,44 @@
 import { OpsData, TypeOps } from "./OpsData"
 import { MapboxGLButtonControl } from "./MapboxGLButtonControl"
-import { Map, Marker, NavigationControl } from "mapbox-gl"
+import { Map, Marker, NavigationControl, Style } from "mapbox-gl"
 import { showPopUp } from "./PopUpAndStats"
+
+const basemaps: {
+  currentBaseMapIndex: number,
+  basemaps: Array<{
+    id: number;
+    name: string;
+    img: string;
+    style: string;
+  }>
+} = {
+  currentBaseMapIndex: 0,
+  basemaps: [{
+    id: 0,
+    name: "SOS Mediterranee",
+    img: "https://raw.githubusercontent.com/CartONG/opsmap-icons/main/basemap_opsmap.png",
+    style: "mapbox://styles/sosmediterranee/ckkdvswwr0ol117t7d91p7wac"
+  },
+  {
+    id: 1,
+    name: "Satellite Imagery",
+    img: "https://raw.githubusercontent.com/CartONG/opsmap-icons/main/basemap_satellite.png",
+    style: "mapbox://styles/mapbox/satellite-v9"
+  },
+  {
+    id: 2,
+    name: "Dark",
+    img: "https://raw.githubusercontent.com/CartONG/opsmap-icons/main/basemap_osm.png",
+    style: "mapbox://styles/mapbox/dark-v10"
+  }
+  ]
+}
 
 export class BaseMap {
   private map!: Map;
   private markers: Marker[] = [];
 
   display (timeFilteredData: OpsData[]): void {
-    const layers = [
-      "mapbox://styles/sosmediterranee/ckkdvswwr0ol117t7d91p7wac",
-      "mapbox://styles/mapbox/satellite-v9",
-      "mapbox://styles/mapbox/dark-v10"
-    ]
-
     // This token was taken from the demo project we need to replace with a real token
     this.map = new Map({
       accessToken: "pk.eyJ1Ijoid2VzbGV5YmFuZmllbGQiLCJhIjoiY2pmMDRwb202MGlzNDJ3bm44cHA3YXZiNCJ9.b2yOf2vbWnWiV7mlsFAywg",
@@ -33,11 +58,10 @@ export class BaseMap {
     this.map.addControl(nav)
 
     /* Event Handlers */
-    let i = 0
     const map2 = this.map
     function nextLayer () {
-      map2.setStyle(layers[i % layers.length])
-      i = i + 1
+      basemaps.currentBaseMapIndex = (basemaps.currentBaseMapIndex + 1) % basemaps.basemaps.length
+      map2.setStyle(basemaps.basemaps[basemaps.currentBaseMapIndex].style)
     }
 
     /* Instantiate new controls with custom event handlers */
