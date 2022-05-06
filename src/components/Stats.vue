181c6dfd2b3a83f2717413ebe73c29fe1ed0a604
Etienne Delclaux <e_delclaux@cartong.org>
2022-05-06 17:30:11 +0200

    feat (i18n): translate stats

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -18,13 +18,12 @@
     <div v-if="displayingStats" id="stats-content" class="flex flex-row justify-around">
       <div class="flex flex-col">
         <span class="icon icon-rescue text-4xl leading-6  text-center text-secondary"/>
-        <p class="text-sm text-main font-bold"><span id="statsNbSurvivor2" class="text-secondary text-lg"/> people
-          rescued</p>
+        <p class="text-sm text-main font-bold"><span id="statsNbSurvivor2" class="text-secondary text-lg"/> {{ $t("stats.peoples") }}</p>
       </div>
       <div class="flex flex-col">
         <span class="icon icon-sosmed-ship text-4xl leading-6 text-center text-secondary"/>
         <p class="text-sm text-center text-main font-bold"><span id="statsNbDays2" class="text-secondary text-lg"/>
-          operational days</p>
+          {{ $t("stats.opDays") }}</p>
       </div>
     </div>
   </div>
@@ -36,50 +35,50 @@
     </h1>
     <div class="flex flex-col pb-6 pl-6 pr-6">
       <span class="icon icon-rescue text-5xl text-center text-secondary"/>
-      <p class="text-main text-xl font-bold mb-5"><span id="statsNbSurvivor" class="text-secondary"/> people rescued</p>
+      <p class="text-main text-xl font-bold mb-5"><span id="statsNbSurvivor" class="text-secondary"/> {{ $t("stats.peoples") }}</p>
       <div class="flex flex-row justify-around mb-5 text-main">
         <div class="flex flex-col">
           <span class="icon icon-female text-5xl text-center"/>
           <span id="statsFemale" class="font-bold text-center"/>
-          <p class="text-xs uppercase text-center">females</p>
+          <p class="text-xs uppercase text-center">{{ $t("stats.females") }}</p>
         </div>
         <div class="flex flex-col">
           <span class="icon icon-male text-5xl text-center"/>
           <span id="statsMale" class="font-bold text-center"/>
-          <p class="text-xs uppercase text-center">males</p>
+          <p class="text-xs uppercase text-center">{{ $t("stats.males") }}</p>
         </div>
         <div class="flex flex-col">
           <span class="icon icon-kid text-5xl text-center"/>
           <span id="statsMinor" class="font-bold text-center"/>
-          <p class="text-xs uppercase text-center">minors</p>
+          <p class="text-xs uppercase text-center">{{ $t("stats.minors") }}</p>
         </div>
       </div>
       <div class="flex flex-row justify-around border-dotted border-secondary border-2 rounded-xl p-4 mb-5">
         <div class="flex flex-row">
           <div class="flex flex-col">
             <span id="statsPregnant" class="font-bold text-right text-secondary"/>
-            <p class="text-3xs uppercase text-right text-secondary">pregnant</p>
+            <p class="text-3xs uppercase text-right text-secondary">{{ $t("stats.pregnant") }}</p>
           </div>
           <span class="icon icon-pregnant text-4xl text-right text-secondary"/>
         </div>
         <div class="flex flex-col">
           <span class="icon text-4xl text-center text-secondary"/>
           <span id="statsUnaccompagnied" class="font-bold text-center text-secondary"/>
-          <p class="text-3xs uppercase text-center text-secondary">unaccompagnied</p>
+          <p class="text-3xs uppercase text-center text-secondary">{{ $t("stats.unaccompagned") }}</p>
         </div>
         <div class="flex flex-row">
           <span class="icon icon-bib text-3xl text-left text-secondary"/>
           <div class="flex flex-col">
             <span id="statsChildren" class="font-bold text-center text-secondary"/>
-            <p class="text-3xs uppercase text-left text-secondary">children</p>
+            <p class="text-3xs uppercase text-left text-secondary">{{ $t("stats.children") }}</p>
           </div>
         </div>
       </div>
       <p class="text-sm text-center text-secondary"><span class="icon icon-planet mr-3"/><span id="statsNationalities"/>
-        nationalities</p>
+        {{ $t("stats.nationalities") }}</p>
       <hr class="border w-1/4 ml-auto mr-auto mt-2 mb-2"/>
       <span class="icon icon-sosmed-ship  text-8xl leading-6 text-center text-secondary mb-2"/>
-      <p class="text-center text-main text-xl font-bold"><span id="statsNbDays" class="text-secondary"/> operational days
+      <p class="text-center text-main text-xl font-bold"><span id="statsNbDays" class="text-secondary"/> {{ $t("stats.opDays") }}
       </p>
     </div>
   </div>
