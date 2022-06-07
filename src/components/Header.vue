99c4c29139e57c02d495d358f98db0c630bfe0c7
Etienne Delclaux <e_delclaux@cartong.org>
2022-06-07 12:51:45 +0200

    add dummy dropdown under cartong logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -75,8 +75,12 @@
           </a>
         </div>
         <div class="flex-grow"></div>
-        <img alt="logo CartONG" class="cursor-pointer inline-block h-6 mr-8" src="../assets/Cartong_logo.png"
-             @click="triggerContributorsPopup"/>
+        <div class="Contributors__container">
+          <img alt="logo CartONG" class="cursor-pointer inline-block h-6 mr-8" src="../assets/Cartong_logo.png"/>
+          <div class="Contributors__content">
+            CONTRIBUTORS
+          </div>
+        </div>
         <div class="flex-none mr-8">
           <a href="#" class="text-center inline-block w-6 bg-white text-black">?</a>
         </div>
@@ -119,4 +123,29 @@ export default defineComponent({
 .up-arrow {
   transform: rotate(180deg);
 }
+
+.Contributors__container {
+  z-index: 9999;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: flex-start;
+  position: relative;
+}
+
+.Contributors__content {
+  display: none;
+  flex-direction: column;
+  position: absolute;
+  top: 100%;
+  right: 0;
+  padding: 0;
+  background-color: aqua;
+}
+
+.Contributors__container:hover > .Contributors__content,
+.Contributors__container:focus > .Contributors__content,
+.Contributors__container:focus-within > .Contributors__content {
+  display: flex;
+}
+
 </style>
