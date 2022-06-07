4eb10e6f4a6030639e7868d28d825751822f68ec
Etienne Delclaux <e_delclaux@cartong.org>
2022-06-07 16:09:09 +0200

    fix(contributors): add data to popup

--- src/components/HeaderContributors.vue
+++ src/components/HeaderContributors.vue
@@ -1,19 +1,58 @@
 <template>
   <div class="Contributors__container">
     <img alt="logo CartONG" class="cursor-pointer inline-block h-6 mr-8" src="../assets/Cartong_logo.png"/>
-    <div class="Contributors__content">
-      CONTRIBUTORS
+    <div class="Contributors__content rounded-3xl overflow-hidden text-main">
+      <div class="Contributors__contentContent m-6">
+        <h1 class="font-bold text-secondary">
+          Contributors
+        </h1>
+        <h2 class="text-main whitespace-nowrap">
+          CartONG volunteers
+        </h2>
+        <hr class="border-dotted border-main border"/>
+        <p class="text-sm align-middle whitespace-nowrap">
+          <span class="icon icon-lifebuoy text-xl mr-3 align-middle"/> <span class="font-bold align-middle">Developer team</span>
+        </p>
+        <div class="flex flex-row gap-4 ">
+          <div class="border border-main"> </div>
+          <div class="flex flex-col align-middle whitespace-nowrap">
+            <p v-for="dev, key in developers" :key="key">
+              {{ dev.name }}
+            </p>
+          </div>
+        </div>
+        <p class="text-sm align-middle whitespace-nowrap">
+          <span class="icon icon-lifebuoy text-xl mr-3 align-middle" /> <span class="font-bold align-middle">Others</span>
+        </p>
+        <div class="flex flex-row gap-4 ">
+          <div class="border border-main"> </div>
+          <div class="flex flex-col align-middle whitespace-nowrap">
+            <p v-for="dev, key in others" :key="key">
+              {{ dev.name }}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue"
+<script setup lang="ts">
+import { Contributor, CONTRIBUTORS } from "@/classes/Contributors"
 
-export default defineComponent({
-  name: "HeaderContributors"
-})
+const compare = (a: Contributor, b: Contributor) => {
+  if (a.isCartONGStaff && !b.isCartONGStaff) {
+    return 1
+  } else if (!a.isCartONGStaff && b.isCartONGStaff) {
+    return -1
+  } else {
+    return a.name.localeCompare(b.name)
+  }
+}
+
+const developers = CONTRIBUTORS.developpers.sort(compare)
+const others = CONTRIBUTORS.others.sort(compare)
 </script>
 
 <style scoped>
@@ -26,13 +65,19 @@ export default defineComponent({
 }
 
 .Contributors__content {
-  display: none;
-  flex-direction: column;
+  display: flex;
   position: absolute;
   top: 100%;
   right: 0;
   padding: 0;
-  background-color: aqua;
+  background-color: #fff;
+}
+
+.Contributors__contentContent {
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: top;
+  align-items: left;
 }
 
 .Contributors__container:hover > .Contributors__content,
@@ -40,5 +85,4 @@ export default defineComponent({
 .Contributors__container:focus-within > .Contributors__content {
   display: flex;
 }
-
 </style>
