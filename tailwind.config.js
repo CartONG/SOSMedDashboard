6caa7354e65554cb45c4cf59a74ab48eb1bd35f8
Jean-Baptiste Regazzoni <jeanbaptiste.regazzoni@protonmail.com>
2022-07-10 13:47:10 +0200

    :arrow_up: chore() upgrade TailWindCSS version to 3 - upgrade TW configuration (unused class + @variants remove + changes for major update)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   mode: "jit",
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
