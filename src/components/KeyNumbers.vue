c843f795c68ac7bd5a6d979f886ef123f1302dbb
NicolasGrosjean <grosjen@hotmail.fr>
2022-12-04 17:19:38 +0100

    Add localisation
    Set texts in French and English

--- src/components/KeyNumbers.vue
+++ src/components/KeyNumbers.vue
@@ -2,21 +2,21 @@
   <div class="bg-main text-white rounded-xl flex-col justify-between position-desktop absolute hidden md:flex">
     <div class="flex flex-col pt-2 pb-6 pl-6 pr-6 gap-2">
       <h1 class='text-secondary text-center'>
-        Since 2016</h1>
+        {{ $t("stats.since") }} 2016</h1>
         <div class='flex flex-row gap-4'>
           <div class='flex flex-col'>
             <p class='text-center text-white text-xl font-bold'>
               <span id="statsNbOperations">
               </span>
             </p>
-            <p>Rescue Operations</p>
+            <p>{{ $t("stats.rescueNb") }}</p>
           </div>
             <div class='flex flex-col'>
             <p class='text-center text-white text-xl font-bold'>
               <span id="statsNbPeopleAssisted">
               </span>
             </p>
-            <p>People assisted</p>
+            <p>{{ $t("stats.peoples") }}</p>
           </div>
           <div>
           </div>
